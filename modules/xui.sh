be43c68faf7fdf1ce97cf74f5ffdbacb5ccf5b0c
SisNaing66 <sisn09050@gmail.com>
2026-02-19 20:48:45 +0630

    Update xui installer

--- modules/xui.sh
+++ modules/xui.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+
+clear
+echo "================================="
+echo "   Installing SisNaing 3X-UI"
+echo "================================="
+
+sleep 2
+
+bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+
+echo ""
+echo "================================="
+echo "   SisNaing 3X-UI Installed!"
+echo "================================="
